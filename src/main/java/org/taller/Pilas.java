60c1ee89a0fbacc921866dd525694459493fe7f0
maria45paula <paulaugggvbk@gmail.com>
2026-07-01 14:00:32 -0500

    Agregando Clase Pilas

---
+++ src/main/java/org/taller/Pilas.java
@@ -0,0 +1,55 @@
+package org.taller;
+
+public class Pilas {
+package Pilas;
+
+    public class Pila {
+        Nodo top;
+        int tamanio;
+
+        public void agregarPila(int valor) {
+            Nodo nuevo = new Nodo(valor);
+            if (top == null) {
+                top = nuevo;
+            } else {
+                nuevo.siguiente = top;
+                top = nuevo;
+            }
+            tamanio++;
+
+        }
+
+        public int peek() {
+            return top.valor;
+        }
+
+        public int pop() {
+            int dato = top.valor;
+            top = top.siguiente;
+
+            tamanio--;
+            return dato;
+        }
+
+        public int getTamanio() {
+            return tamanio;
+        }
+
+        public void imprimirLista() {
+            if (this.top != null) {
+                Nodo puntero = top;
+                while (puntero != null) {
+                    System.out.println(puntero.valor);
+                    puntero = puntero.siguiente;
+                }
+            } else {
+                System.out.println("Lista vacia");
+            }
+        }
+
+        public boolean isEmpty() {
+            return tamanio == 0;
+        }
+    }
+
+}
